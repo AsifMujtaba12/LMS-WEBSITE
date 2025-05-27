8b23cb2a72bdd319b0cda2b34ccdb53f67487872
[firstname lastname] <asifmujtaba8888@gmail.com>
2025-05-27 18:00:14 +0530

    update connetion of db

--- server/server.js
+++ server/server.js
@@ -21,9 +21,10 @@ app.use(express.urlencoded({ extended: true })); // Parse URL-encoded form data
 app.get('/', (req, res) => {
     res.send('Hello LMS Server is Working!');
 });
-app.post('/clerk', express.json(), clerkWebhooks)
+app.post('/clerk', express.raw({ type: 'application/json' }), clerkWebhooks);
+
 //connect database
- await connectDB()
+connectDB()
 // connectCloudinary();
 app.listen(PORT, (req, res)=>{
     console.log(`Server is running on port ${PORT}`);  // log the server started message  on console  when server is running  on specified port 4000.  'PORT' is environment variable set by heroku during deployment.  If not set, server will run on port 4000.  'dotenv' package is used to load environment variables from a .env file.  'cors' package is used to enable cross-origin resource sharing.  'express.json()' is used to parse incoming request bodies into JSON.  'app.get' is used to define a GET route for the homepage.  'app.listen' is used to start the server.  'console.log' is used to log the message to the console.  'express' is a popular web application framework for Node.js.  'cors' is a middleware that enables CORS.  'dotenv' is a package that loads environment variables from a
